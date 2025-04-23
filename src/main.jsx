0c53bf6755016dcd36b0646deb13f024987d5a43
jesusadrian14 <jesuscasierra928@gmail.com>
2025-04-23 13:46:07 -0500

    agregacion del navar Fecha: 23/04/2025

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import './index.css'; // Estilos globales
 import Home from './proyecto_pomodoro_react/pages/home.jsx'; // Página principal
 import Settings from './proyecto_pomodoro_react/components/settings.jsx'; // Contexto de configuración
 import Timer from './proyecto_pomodoro_react/components/timer.jsx'; // Contexto del temporizador
-import statsPage from './proyecto_pomodoro_react/pages/stats_Page.jsx';
-import reportesPage from './proyecto_pomodoro_react/pages/reportes_Page.jsx';
-import settingPage from './proyecto_pomodoro_react/pages/settings_Page.jsx';
+import StatsPage from './proyecto_pomodoro_react/pages/stats_Page.jsx';
+import ReportesPage from './proyecto_pomodoro_react/pages/reportes_Page.jsx';
+import SettingPage from './proyecto_pomodoro_react/pages/settings_Page.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Settings>
             <Timer>
-                <reportesPage />
-                <statsPage />
-                <settingPage />
+                <ReportesPage />
+                <StatsPage />
+                <SettingPage />
                 <Home />
             </Timer>
         </Settings>
