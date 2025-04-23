386980f6e110315c6e0d1f219da553a34c9357d2
jesusadrian14 <jesuscasierra928@gmail.com>
2025-04-22 23:59:29 -0500

    Actualizo renderizados en main.jsx para mostrar la carpeta que contiene el proyecto pomodoro

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Estilos globales
-import Backend from './backend/backend.jsx';
-// Renderiza el componente App en el elemento root
+import Home from './proyecto_pomodoro_react/pages/home.jsx'; // Página principal
+import Settings from './proyecto_pomodoro_react/components/settings.jsx'; // Contexto de configuración
+import Timer from './proyecto_pomodoro_react/components/timer.jsx'; // Contexto del temporizador
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Backend />
+        <Settings>
+            <Timer>
+                <Home />
+            </Timer>
+        </Settings>
     </React.StrictMode>
 );
