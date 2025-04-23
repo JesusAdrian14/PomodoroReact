93b7ec753919c38f1e550999462c3187c60a55a5
jesusadrian14 <jesuscasierra928@gmail.com>
2025-04-23 00:22:09 -0500

    Actualizo renderizados en main.jsx para mostrar los componentes del nav

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import './index.css'; // Estilos globales
 import Home from './proyecto_pomodoro_react/pages/home.jsx'; // Página principal
 import Settings from './proyecto_pomodoro_react/components/settings.jsx'; // Contexto de configuración
 import Timer from './proyecto_pomodoro_react/components/timer.jsx'; // Contexto del temporizador
+import statsPage from './proyecto_pomodoro_react/pages/stats_Page.jsx';
+import reportesPage from './proyecto_pomodoro_react/pages/reportes_Page.jsx';
+import settingPage from './proyecto_pomodoro_react/pages/settings_Page.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Settings>
             <Timer>
+                <reportesPage />
+                <statsPage />
+                <settingPage />
                 <Home />
             </Timer>
         </Settings>
