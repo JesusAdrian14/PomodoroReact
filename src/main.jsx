a163c0b8062c1672a49f96c72ee6b7b8d44b437b
JesusAdrian14 <jesuscasierra928@gmail.com>
2025-05-12 16:06:20 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import './index.css'; // Estilos globales
+/*
 import Home from './proyecto_pomodoro_react/pages/home.jsx'; // Página principal
 import Settings from './proyecto_pomodoro_react/components/settings.jsx'; // Contexto de configuración
 import Timer from './proyecto_pomodoro_react/components/timer.jsx'; // Contexto del temporizador
 import StatsPage from './proyecto_pomodoro_react/pages/stats_Page.jsx';
 import ReportesPage from './proyecto_pomodoro_react/pages/page_btn_reporte.jsx';
 import SettingPage from './proyecto_pomodoro_react/pages/page_btn_settings.jsx';
-
+*/
+import App from './medidorDePeso/App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Settings>
